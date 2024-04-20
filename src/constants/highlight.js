500d3bd3bfb54e362de8033468168ece11ac449c
zhanghui <1257126113@qq.com>
2024-04-20 17:20:26 +0800

    change  dir

---
+++ src/constants/highlight.js
@@ -0,0 +1,6 @@
+const highlightSequence = ['#FF9632', '#FFCC00', '#FF9632'].join(';'); // yellow - deep orange
+export const highlightSpanSequence = highlightSequence;
+export const highlightArcSequence = highlightSequence;
+export const highlightTextSequence = highlightSequence;
+export const highlightDuration = '2s';
+export const highlightMatchSequence = '#FFFF00';
